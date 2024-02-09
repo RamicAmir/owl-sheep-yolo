04d3c545fff5525a869d0d5f02583bc50a61a09d
Amir Ramic <amir_ramic@msn.com>
2024-02-09 09:48:11 +0100

    added comments

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import fiftyone.zoo as foz
 
 dataset = fiftyone.zoo.load_zoo_dataset(
               "open-images-v7",
-              split="train",
+              split="train",  # Pictures meant for training model
               label_types=["detections", "segmentations", "points"],
-              classes=["Owl", "Sheep"],
-              max_samples=500,
+              classes=["Owl", "Sheep"],  # Classes for animals
+              max_samples=500,  # Quantity of dataset in pictures ex. 500 pictures
           )
-session = fo.launch_app(dataset, port=5151)
-session.wait()
+session = fo.launch_app(dataset, port=5151)  # Create localhost session to explore dataset
+session.wait()  # Prevent termination of localhost until quit
